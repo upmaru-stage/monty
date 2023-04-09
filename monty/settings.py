c49db4c3e3dc75d52076f9b6109de0e477b09061
Zack Siri <me@zacksiri.dev>
2023-04-09 08:57:40 +0700

    Setup migrations, gitignore
    + Remove cache
    
    Signed-off-by: Zack Siri <me@zacksiri.dev>

--- monty/settings.py
+++ monty/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'monty.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'monty_dev',
+        'USER': 'zacksiri',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
